db599763c46ec2d6d1f94dfac433ff5d10b98366
Nick Jones <nrjones8@gmail.com>
2019-09-20 08:44:09 -0400

    parse 2019 PDF naming changes

--- datacleaning/cdcr_util.py
+++ datacleaning/cdcr_util.py
@@ -1,7 +1,18 @@
 import re
 
 def get_year_month_from_pdf_name(pdf_name):
-    m = re.match('TPOP1Ad(\d{2})(\d{2}).pdf', pdf_name)
+    # TPOP1Ad1810.pdf --> example of pre-2019
+    # Tpop1d1905.pdf --> example of 2019 and later
+    regex_pre_2019 = 'TPOP1Ad(\d{2})(\d{2}).pdf'
+    regex_2019_later = 'Tpop1d(\d{2})(\d{2}).pdf'
+    m = re.match(regex_pre_2019, pdf_name)
+
+    if m is None:
+        m = re.match(regex_2019_later, pdf_name)
+
+    if m is None:
+        raise Exception('Could not parse year and month from pdf_name "{}"'.format(pdf_name))
+
     year = m.group(1)
     if int(year) < 28:
         # Hopefully by 2028, these datasets will not be in PDFs.
--- test/cdcr_util_test.py
+++ test/cdcr_util_test.py
@@ -6,6 +6,12 @@ class CdcrUtilTest(unittest.TestCase):
     def test_parses_pdf_name(self):
         self.assertEquals(('2011', '08'), get_year_month_from_pdf_name('TPOP1Ad1108.pdf'))
         self.assertEquals(('1998', '08'), get_year_month_from_pdf_name('TPOP1Ad9808.pdf'))
+        self.assertEquals(('2018', '12'), get_year_month_from_pdf_name('TPOP1Ad1812.pdf'))
+        self.assertEquals(('2017', '11'), get_year_month_from_pdf_name('TPOP1Ad1711.pdf'))
+
+    def test_parses_2019_names(self):
+        self.assertEquals(('2019', '05'), get_year_month_from_pdf_name('Tpop1d1905.pdf'))
+        self.assertEquals(('2019', '07'), get_year_month_from_pdf_name('Tpop1d1907.pdf'))
 
 if __name__ == '__main__':
     unittest.main()
